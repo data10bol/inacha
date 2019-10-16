bdc0935d373bf391058e234c44131dcc5c0d4f12
JorgRios <www.fer_gha@hotmail.com>
2019-10-16 09:45:16 -0400

    Arreglado el bug de las cartas en el inicio

--- resources/views/layouts/partials/cards.blade.php
+++ resources/views/layouts/partials/cards.blade.php
@@ -17,12 +17,37 @@
       <div class="col-3">
         <div class="card card-dark card-outline">
           <div class="card-header">
+            @php
+                $ban = false;
+            @endphp
             @if($type == 'operation')
               <h3 class="m-0">Operación
                 {{   $item->action->goal->code }}.{{
                                 $item->action->code }}.{{
                                 $item->code }}
               </h3>
+              @php
+                $ar = quantity_exe($item->id,'operation',activemonth());
+              @endphp
+                @if ($ar[0]==$ar[1])
+                <i class="fa fa-check-square text-success"></i>
+                @php
+                  $month = 'm' . activemonth();                            
+                @endphp
+                  @if(!empty($item->poas()->where('month', activemonth())->first()->$month))
+                    <i class="fa fa-check-square text-success"></i>
+                    @php
+                    $ar[3] = false;
+                    //ok
+                    @endphp
+                  @endif
+                @else
+                <span tabindex="0" role="button" class="badge badge-{{$ar[2]}}" data-toggle="popover" data-trigger="focus" title="Pendientes de Ejecución" data-content="{{$ar[4]}}" ><small>{{$ar[1]}} / {{$ar[0]}}</small></span> Tareas Pendientes
+                @php
+                    $ban = true;
+                @endphp
+                @endif
+
             @elseif($type == 'task')
               <h3 class="m-0">TAREA
                 {{   $item->operation->action->goal->code }}.{{
@@ -53,11 +78,12 @@
               </p>
             @endif
             {!! Form::button('Avance',
-              array('title' => 'Registrar',
-                      'type' => 'button',
-                      'class' => 'btn btn-danger',
-                      'onclick'=>'window.location.href="/execution/'.$type.'/'.
-                      Hashids::encode($item->id).'/edit"', ))
+                array('title' => 'Registrar',
+                        'type' => 'button',
+                        'class' => 'btn btn-danger',
+                        'disabled' => $ban,
+                        'onclick'=>'window.location.href="/execution/'.$type.'/'.
+                        Hashids::encode($item->id).'/edit"', ))
             !!}
           </div>
         </div>
@@ -80,3 +106,13 @@
     </div>
   @endforelse
 </div>
+@section('scripts')
+<script>
+  $(document).ready(function(){
+    $('[data-trigger="focus"]').popover({ 
+          html:true ,
+          trigger: 'focus'
+        });  
+  });
+  </script>
+@endsection
